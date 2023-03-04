4d81bfd9f21fcdad7adaa6ce762159276c100840
jorjepadre <iamzhanaidar.mukanov@gmail.com>
2023-03-04 12:05:14 +0800

    Project Structure

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        This is my app
+      </Text>
+    </View>
+  )
+}
+
+export default App;
